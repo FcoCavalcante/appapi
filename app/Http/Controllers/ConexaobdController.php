4a83ec3310cc3b51a5a6b4c69f85139794712876
Francisco <francisco.cavalcante2@etec.sp.gov.br>
2025-08-22 22:42:27 -0300

    primeiro commit

---
+++ app/Http/Controllers/ConexaobdController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\conexaobd;
+use Illuminate\Http\Request;
+
+class ConexaobdController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(conexaobd $conexaobd)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, conexaobd $conexaobd)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(conexaobd $conexaobd)
+    {
+        //
+    }
+}
---
+++ app/Models/conexaobd.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class conexaobd extends Model
+{
+    protected $fillable = [
+        'name',
+        'preco',
+        'marca'
+    ];
+}
